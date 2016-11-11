156a096fb1ca1a561fb96252ed9652aec3e596d6
Hüseyin Mert <huseyinin@gmail.com>
2016-11-11 08:53:10 +0300

    #1642 #1628 #1627 #1603

---
+++ src/Ojs/CoreBundle/Helper/CitationHelper.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Ojs\CoreBundle\Helper;
+
+
+class CitationHelper
+{
+
+    public function risExporter(){}
+    public function endNoteExporter(){}
+    public function pubmedXmlExporter(){}
+    public function jatsExporter(){}
+}
